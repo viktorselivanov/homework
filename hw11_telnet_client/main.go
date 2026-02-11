6b810d77c3189db355f53e35d939ef18ef89b9a1
sad <sad@bb.bb>
2026-02-11 15:31:52 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -12,25 +12,25 @@ func main() {
 
 	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
 
-	// Пытаемся подключиться к серверу.
 	if err := client.Connect(); err != nil {
-		// Если подключение не удалось, выводим ошибку в stderr и завершаем программу
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	// Конкурентно запускаем горутину для отправки данных с stdin в сокет.
+	done := make(chan struct{}, 2)
+
 	go func() {
-		if err := client.Send(); err != nil {
-			// Если произошла ошибка при отправке данных, выводим её в stderr
-			fmt.Fprintln(os.Stderr, err)
-		}
-		client.Close()
+		_ = client.Send()
+		done <- struct{}{}
 	}()
 
-	if err := client.Receive(); err != nil {
-		fmt.Fprintln(os.Stderr, "Receive finished:", err) // Обычно Receive завершится, когда сервер закроет соединение
-	}
+	go func() {
+		_ = client.Receive()
+		done <- struct{}{}
+	}()
 
-	client.Close()
+	// ждём завершения одной из сторон
+	<-done
+
+	_ = client.Close()
 }
