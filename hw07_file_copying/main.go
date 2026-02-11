f8e9d2475afcf3854d1e9e26d748b365a8eef37a
sad <sad@bb.bb>
2026-02-11 10:08:37 +0300

    HW7 is completed

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -18,5 +20,14 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+
+	if from == "" || to == "" {
+		fmt.Fprintln(os.Stderr, "both -from and -to must be specified")
+		os.Exit(1)
+	}
+
+	if err := Copy(from, to, offset, limit); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
 }
